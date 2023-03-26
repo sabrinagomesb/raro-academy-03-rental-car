4b0b202574fb37dc98ebf826efdf63104744e307
Sabrina Gomes Bezerra <sabrinagbdev@gmail.com>
2023-03-26 18:09:44 -0300

    feat: implementa classe Cliente

---
+++ lib/rarorental/entidades/cliente.rb
@@ -0,0 +1,23 @@
+class Cliente
+  attr_accessor :cpf, :nome
+
+  def initialize(cpf, nome)
+    valida(cpf, nome)
+    @cpf = cpf
+    @nome = nome
+  end
+
+  def atualiza_cadastro(cpf, nome)
+    valida(cpf, nome)
+    @cpf = cpf
+    @nome = nome
+  end
+
+  private
+
+  def valida(cpf, nome)
+    raise ErroValidacao.new("Não pode ser inicializado um cliente sem nome") if nome.nil? || nome.empty?
+
+    raise ErroValidacao.new("Um cliente deve ter cpf com 11 caracteres numéricos") unless cpf.match?(/^\d{11}$/)
+  end
+end
