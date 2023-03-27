2ac059076a8667277a05b478928cf616f581a2f2
Sabrina Gomes Bezerra <sabrinagbdev@gmail.com>
2023-03-26 21:40:19 -0300

    feat: implementa classe Reserva

---
+++ lib/rarorental/entidades/reserva.rb
@@ -0,0 +1,23 @@
+class Reserva
+  attr_accessor :cliente, :veiculo, :data_inicio, :data_fim, :preco
+
+  def initialize(cliente, veiculo, data_inicio, data_fim)
+    @cliente = cliente
+    @veiculo = veiculo
+    @data_inicio = data_inicio
+    @data_fim = data_fim
+    @preco = calcula_preco
+  end
+
+  def calcula_preco
+    total_dias = (@data_fim - @data_inicio).to_i
+    diaria_padrao = @veiculo.diaria_padrao
+    diaria_desconto = @veiculo.diaria_desconto
+
+    if total_dias <= 6
+      diaria_padrao * total_dias
+    else
+      diaria_desconto * total_dias
+    end
+  end
+end
