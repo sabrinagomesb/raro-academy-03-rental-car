836850385d6487534f184b3f514a713632916c7e
Sabrina Gomes Bezerra <sabrinagbdev@gmail.com>
2023-03-27 20:25:32 -0300

    build: instala byebug

--- lib/rarorental.rb
+++ lib/rarorental.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
-
+require "byebug"
+require "date"
 require_relative "rarorental/version"
 
 require_relative "./rarorental/entidades/cliente"
 require_relative "./rarorental/entidades/estoque"
+require_relative "./rarorental/entidades/locacao"
 require_relative "./rarorental/entidades/reserva"
 require_relative "./rarorental/entidades/pagamento"
 require_relative "./rarorental/entidades/veiculo"
