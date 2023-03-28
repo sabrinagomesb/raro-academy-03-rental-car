342b5ee0546dd2ffb654b2c9759d110f896eac6b
Sabrina Gomes Bezerra <sabrinagbdev@gmail.com>
2023-03-28 00:24:36 -0300

    feat: implementa metodos disponivel? e busca_disponibilidade na classe Veiculo

--- lib/rarorental/entidades/veiculo.rb
+++ lib/rarorental/entidades/veiculo.rb
@@ -1,6 +1,7 @@
 class Veiculo
   attr_accessor :placa, :fabricante, :modelo, :ano,
-                :diaria_padrao, :diaria_desconto, :disponivel
+                :diaria_padrao, :diaria_desconto,
+                :reservas, :locacoes
 
   def initialize(placa, fabricante, modelo, ano, diaria_padrao)
     valida(placa)
@@ -10,12 +11,31 @@ class Veiculo
     @ano = ano
     @diaria_padrao = diaria_padrao.to_f
     @diaria_desconto = (diaria_padrao * 0.9).to_f
-    @disponivel = true
+    @reservas = []
+    @locacoes = []
+  end
+
+  def disponivel?(data_inicio, data_fim)
+    raise ErroValidacao.new("Os parâmetros devem ser datas") if data_inicio.class != Date || data_fim.class != Date
+
+    busca_disponibilidade(@locacoes, data_inicio, data_fim) &&
+    busca_disponibilidade(@reservas, data_inicio, data_fim)
   end
 
   private
 
   def valida(placa)
     raise ErroValidacao.new("Placa deve conter 3 digitos alfanumericos seguidos de hifen e 4 digitos alfanumericos. Ex: CAR-1234") unless placa.match?(/^[[:alnum:]]{3}-[[:alnum:]]{4}$/)
+  end
+
+  def busca_disponibilidade(lista, data_inicio, data_fim)
+    lista.each do |item|
+      if data_inicio > item.data_fim || data_fim < item.data_inicio
+        return true
+      elsif data_inicio.between?(item.data_inicio, item.data_fim) || data_fim.between?(item.data_inicio, item.data_fim)
+        return false
+      end
+    end
+    true
   end
 end
--- spec/veiculo_spec.rb
+++ spec/veiculo_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe Veiculo do
         expect(veiculo.ano).to eq(2020)
         expect(veiculo.diaria_padrao).to eq(100)
         expect(veiculo.diaria_desconto).to eq(90)
-        expect(veiculo.disponivel).to eq(true)
+        expect(veiculo.reservas).to eq([])
+        expect(veiculo.locacoes).to eq([])
       end
     end
 
@@ -24,6 +25,51 @@ RSpec.describe Veiculo do
         expect { Veiculo.new("CAR1-234", "Fiat", "Mobi", 2020, 100) }.to raise_error(ErroValidacao, mensagem_erro)
 
         expect { Veiculo.new("1234", "Fiat", "Mobi", 2020, 100) }.to raise_error(ErroValidacao, mensagem_erro)
+      end
+    end
+  end
+
+  describe "Disponivel" do
+    let(:veiculo) { Veiculo.new("CAR-1234", "Fiat", "Mobi", 2020, 100) }
+    let(:cliente) { Cliente.new("11122233300", "Joao Silva") }
+    let(:veiculo) { Veiculo.new("CAR-1234", "Fiat", "Mobi", 2020, 100) }
+    let(:data_inicio) { Date.new(2022, 01, 15) }
+    let(:data_fim) { Date.new(2022, 01, 20) }
+    let(:reserva) { Reserva.new(cliente, veiculo, data_inicio, data_fim) }
+    let(:locacoes) { Locacao.new(cliente, veiculo, data_inicio, data_fim) }
+
+    context "Quando dado parametros validos" do
+      it "Deve retornar que veiculo possui dispobilidade nas datas indicadas" do
+        veiculo.reservas << reserva
+        veiculo.locacoes << reserva
+        data_inicio_1 = Date.new(2022, 02, 15)
+        data_fim_1 = Date.new(2022, 02, 20)
+        data_inicio_2 = Date.new(2022, 01, 12)
+        data_fim_2 = Date.new(2022, 01, 14)
+
+        expect(veiculo.disponivel?(data_inicio_1, data_fim_1)).to eq(true)
+        expect(veiculo.disponivel?(data_inicio_2, data_fim_2)).to eq(true)
+      end
+
+      it "Deve retornar que veiculo NÃO possui dispobilidade nas reservas" do
+        veiculo.reservas << reserva
+        data_inicio_1 = Date.new(2022, 01, 10)
+        data_fim_1 = Date.new(2022, 01, 15)
+        data_inicio_2 = Date.new(2022, 01, 20)
+        data_fim_2 = Date.new(2022, 01, 30)
+
+        expect(veiculo.disponivel?(data_inicio, data_fim)).to eq(false)
+        expect(veiculo.disponivel?(data_inicio_1, data_fim_1)).to eq(false)
+        expect(veiculo.disponivel?(data_inicio_2, data_fim_2)).to eq(false)
+      end
+    end
+
+    context "Quando dado parametros inválidos" do
+      it "Deve lançar erro se os parametros não forem datas" do
+        mensagem_erro = "Os parâmetros devem ser datas"
+
+        expect { veiculo.disponivel?("2022-01-01", "2022-01-05") }.to raise_error(ErroValidacao, mensagem_erro)
+        expect { veiculo.disponivel?(20220101, 20220105) }.to raise_error(ErroValidacao, mensagem_erro)
       end
     end
   end
