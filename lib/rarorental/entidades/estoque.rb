163d123b74dacab68905979852cbec0eab48adfd
Sabrina Gomes Bezerra <sabrinagbdev@gmail.com>
2023-03-28 00:22:34 -0300

    feat: implementa metodo de veiculos_disponiveis na classe Estoque

--- lib/rarorental/entidades/estoque.rb
+++ lib/rarorental/entidades/estoque.rb
@@ -5,6 +5,18 @@ class Estoque
     @veiculos = []
   end
 
+  def veiculos_disponiveis(data_inicio, data_fim)
+    raise ErroValidacao.new("Os parâmetros devem ser datas") if data_inicio.class != Date || data_fim.class != Date
+
+    veiculos_disponiveis = []
+
+    @veiculos.each do |veiculo|
+      veiculos_disponiveis << veiculo if veiculo.disponivel?(data_inicio, data_fim)
+    end
+
+    veiculos_disponiveis
+  end
+
   def cadastra_veiculo(veiculo)
     raise ErroValidacao.new("O parametro informado não é um veículo") if veiculo.class != Veiculo
 
@@ -14,9 +26,9 @@ class Estoque
   end
 
   def remove_veiculo(veiculo)
-    raise ErroValidacao.new("Veículo não está disponível no estoque, por isso não pode ser removido") if veiculo.disponivel == false
-
     raise ErroValidacao.new("Veículo informado não existe no estoque.") unless @veiculos.include?(veiculo)
+
+    raise ErroValidacao.new("Veículo não está disponível no estoque, por isso não pode ser removido") unless veiculo.reservas.empty? && veiculo.locacoes.empty?
 
     @veiculos.delete(veiculo)
   end
--- spec/estoque_spec.rb
+++ spec/estoque_spec.rb
@@ -30,9 +30,14 @@ RSpec.describe Estoque do
 
   describe "Remove veiculo" do
     let(:estoque) { Estoque.new }
+    let(:cliente) { Cliente.new("11122233300", "Joao Silva") }
+    let(:veiculo) { Veiculo.new("CAR-1234", "Fiat", "Mobi", 2020, 100) }
+    let(:data_inicio) { Date.new(2022, 01, 15) }
+    let(:data_fim) { Date.new(2022, 01, 20) }
+    let(:reserva) { Reserva.new(cliente, veiculo, data_inicio, data_fim) }
+
     context "Quando dado parametros válidos" do
       it "Deve remover veiculo do estoque" do
-        veiculo = Veiculo.new("RAR-1234", "Fiat", "Mobi", 2021, 100)
         estoque.cadastra_veiculo(veiculo)
         expect(estoque.veiculos.include?(veiculo)).to eq(true)
 
@@ -43,9 +48,13 @@ RSpec.describe Estoque do
 
     context "Quando dado parametros inválidos" do
       it "Deve lançar erro se o veículo estiver em locação" do
-        #arrange
         veiculo = Veiculo.new("RAR-1256", "Fiat", "Mobi", 2021, 100)
-        veiculo.disponivel = false
+        cliente = Cliente.new("11122233300", "Joao Silva")
+        data_inicio = Date.new(2022, 01, 15)
+        data_fim = Date.new(2022, 01, 20)
+        reserva = Reserva.new(cliente, veiculo, data_inicio, data_fim)
+
+        veiculo.reservas << reserva
         estoque.cadastra_veiculo(veiculo)
         expect(estoque.veiculos.include?(veiculo)).to eq(true)
 
@@ -64,6 +73,43 @@ RSpec.describe Estoque do
 
         mensagem_erro = "Veículo informado não existe no estoque."
         expect { estoque.remove_veiculo(veiculo) }.to raise_error(ErroValidacao, mensagem_erro)
+      end
+    end
+  end
+
+  describe "Veiculos disponiveis" do
+    let(:estoque) { Estoque.new }
+    let(:cliente) { Cliente.new("11122233300", "Joao Silva") }
+    let(:veiculo) { Veiculo.new("BUS-1234", "Fiat", "Uno", 2015, 50) }
+    let(:data_inicio) { Date.new(2022, 01, 01) }
+    let(:data_fim) { Date.new(2022, 01, 05) }
+    let(:reserva) { Reserva.new(cliente, veiculo, data_inicio, data_fim) }
+    # let(:locacao) { Locacao.new(cliente, veiculo, data_inicio, data_fim) }
+
+    context "Quando dado parametros válidos" do
+      it "Deve retornar lista de veiculos disponíveis" do
+        veiculo_1 = Veiculo.new("RAR-1234", "Fiat", "Mobi", 2021, 100)
+        veiculo_2 = Veiculo.new("ABC-4444", "Fiat", "Argo", 2022, 300)
+        estoque.cadastra_veiculo(veiculo)
+        estoque.cadastra_veiculo(veiculo_1)
+        estoque.cadastra_veiculo(veiculo_2)
+
+        veiculo.reservas << reserva
+
+        veiculos = estoque.veiculos_disponiveis(data_inicio, data_fim)
+
+        expect(veiculos.include?(veiculo)).to eq(false)
+        expect(veiculos.include?(veiculo_1)).to eq(true)
+        expect(veiculos.include?(veiculo_2)).to eq(true)
+      end
+    end
+
+    context "Quando dado parametros inválidos" do
+      it "Deve lançar erro se os parametros não forem datas" do
+        mensagem_erro = "Os parâmetros devem ser datas"
+
+        expect { estoque.veiculos_disponiveis("2022-01-01", "2022-01-05") }.to raise_error(ErroValidacao, mensagem_erro)
+        expect { estoque.veiculos_disponiveis(20220101, 20220105) }.to raise_error(ErroValidacao, mensagem_erro)
       end
     end
   end
